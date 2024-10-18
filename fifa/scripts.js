25e8215bbb1fb0bc520b72d585b0dffeb5da616a
fullsdmdeveloper <fullsdmdeveloper@gmail.com>
2024-10-17 23:54:48 -0500

    send

--- fifa/scripts.js
+++ fifa/scripts.js
@@ -7,3 +7,25 @@ menuIcon.addEventListener('click', () => {
     // Alternar la clase 'active' en la lista de navegación
     navLinks.classList.toggle('active');
 });
+
+
+
+// Seleccionamos los elementos que queremos revelar
+const revealElements = document.querySelectorAll('.reveal');
+
+// Configuramos el Intersection Observer
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // Agregamos la clase "visible" cuando el elemento es visible
+            entry.target.classList.add('visible');
+            // Dejamos de observar el elemento una vez que ha aparecido
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.5 }); // Umbral del 50% para activar la animación
+
+// Observamos cada uno de los elementos
+revealElements.forEach(element => {
+    observer.observe(element);
+});
