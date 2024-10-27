4cf736950de44b50b28b98ab8a32f65c83949c50
fullsdmdeveloper <fullsdmdeveloper@gmail.com>
2024-10-27 14:39:23 -0500

    send

--- script/pages/index.js
+++ script/pages/index.js
@@ -1,7 +1,9 @@
 function openNav() {
   document.getElementById("nav").style.display = "block";
+  document.body.style.overflow = "hidden";
 }
 
 function closeNav() {
   document.getElementById("nav").style.display = "none";
+  document.body.style.overflow = "";
 }
